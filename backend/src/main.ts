7b5f3ac36419e5ac6babf931a9d262e373b4bcb6
Augusto <cm1803419@gmail.com>
2026-04-27 02:34:17 -0500

    Despliegue saas 2.0

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { json, urlencoded } from 'express';
+import express, { json, urlencoded } from 'express';
 import helmet from 'helmet';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
