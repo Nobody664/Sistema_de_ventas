13467a65669b94516e164fce3b93271b80fcd7a4
Augusto <cm1803419@gmail.com>
2026-06-03 21:02:36 -0500

    refix 2.0.5

--- backend/src/modules/email/email.module.ts
+++ backend/src/modules/email/email.module.ts
@@ -3,9 +3,11 @@ import { BullModule } from '@nestjs/bullmq';
 import { EmailService } from './email.service';
 import { EmailProcessor } from './email.processor';
 
+const hasRedis = !!process.env.REDIS_URL;
+
 @Module({
-  imports: [BullModule.registerQueue({ name: 'email' })],
-  providers: [EmailService, EmailProcessor],
+  imports: hasRedis ? [BullModule.registerQueue({ name: 'email' })] : [],
+  providers: [EmailService, ...(hasRedis ? [EmailProcessor] : [])],
   exports: [EmailService],
 })
 export class EmailModule {}
